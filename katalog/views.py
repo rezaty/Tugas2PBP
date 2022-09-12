f122c989b6b3d7c915886a801230767b4445bbb0
rezataufiq56 <rezataufiq56@gmail.com>
2022-09-12 22:47:18 +0700

    Pengumpulan Tugas PBP 2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
+from multiprocessing import context
 from django.shortcuts import render
-
+from katalog.models import CatalogItem
+def show_katalog(request):
+    data_item_katalog = CatalogItem.objects.all()
+    context = {
+        'list_katalog' : data_item_katalog,
+        'nama' : 'Reza Taufiq Yahya',
+        'NPM' : '2106654183'  ,  
+    }
+    return render(request, "katalog.html", context)
 # TODO: Create your views here.
