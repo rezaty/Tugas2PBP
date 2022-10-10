05ec898f11e422e5d81e5c3ece41fd3a8adcf1ca
rezataufiq56 <rezataufiq56@gmail.com>
2022-10-11 06:55:43 +0700

    Update urls.py project django

--- project_django/urls.py
+++ project_django/urls.py
@@ -21,7 +21,6 @@ urlpatterns = [
     path('', include('example_app.urls')),
     path('katalog/',include ('katalog.urls')),
     path('mywatchlist/',include('mywatchlist.urls')),
-    path('wishlist/', include('wishlist.urls')),
     path ('todolist/',include ('todolist.urls')),
 ]
 
